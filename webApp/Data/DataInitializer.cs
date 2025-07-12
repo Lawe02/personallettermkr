82d43889f8cdaf290b81acd18b4d729103a26694
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-07-13 01:19:12 +0200

    datainitializer for user

---
+++ webApp/Data/DataInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+
+namespace webApp.Data
+{
+    public class DataInitializer
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager _manager;
+    }
+}
