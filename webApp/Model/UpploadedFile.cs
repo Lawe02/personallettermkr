8c81d6c21a452de4fa46526930ad40a03c7e2e66
Lawe02 <89772680+Lawe02@users.noreply.github.com>
2025-07-14 18:46:00 +0200

    class for storing files dine

---
+++ webApp/Model/UpploadedFile.cs
@@ -0,0 +1,10 @@
+﻿namespace webApp.Model
+{
+    public class UpploadedFile
+    {
+        public int Id { get; set; } 
+        public required string FileName { get; set; } = null!;
+        public required string ContentType { get; set; }
+        public required byte[] Data { get; set; } 
+    }
+}
